2ea4d6b344b97ea4849dc634f4e761e632470086
Sami Jakonen <sami.jakonen@iki.fi>
2023-07-14 14:56:25 +0300

    Use Arch

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # My Linux dotfiles and install scripts
-These work on my machine but might not work on yours without major modifications. Use them only as inspiration. Run the script `bin/install.sh` to install these dotfiles for Fedora 36 with the Gnome DE.
+These work on my machine but might not work on yours without major modifications. Use them only as inspiration.
 
 ## Installing packages and configuring the system
 
-1. Install latest Fedora (full install) with LVM and LUKS.
+1. Install Arch with Gnome with LVM and LUKS.
 2. Import SSH keys.
 ```
 mkdir /home/sami/.ssh
@@ -14,31 +14,3 @@ chmod 600 /home/sami/.ssh/id_ed25519
 ```
 3. Clone this repo: `git clone git@github.com/sjaks/.files.git`.
 4. Run `./bin/install.sh`.
-
-## List of good Gnome extensions
-[Blur my Shell](https://extensions.gnome.org/extension/3193/blur-my-shell/)   
-[Tray Icons Reloaded](https://extensions.gnome.org/extension/2890/tray-icons-reloaded/)
-
-## Installing Nvidia drivers and fixing some configurations
-1. Install the drivers `sudo dnf update && sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda`. (needs RPM Fusion repos)
-2. Blacklist nouveau drivers `echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf`.
-3. Append `rd.driver.blacklist=nouveau nvidia-drm.modeset=1` to `GRUB_CMDLINE_LINUX` in `/etc/default/grub`. (Fixes GDM crashing)
-4. Uncomment `WaylandEnable=false` in `/etc/gdm/custom.conf` (Wayland is very buggy on Nvidia still).
-5. Change systemd target with `sudo systemctl set-default graphical.target`. (if system boots to tty)
-6. Remake Grub2 configs with `sudo grub2-mkconfig -o /etc/grub2-efi.cfg` and `sudo grub2-mkconfig -o /etc/grub2.cfg`
-7. Do a `reboot`
-
-## Fixing laptop battery drainage issues
-1. Append `mem_sleep_default=deep` to `GRUB_CMDLINE_LINUX` in `/etc/default/grub`.
-2. Remake Grub2 configs with `grub2-mkconfig -o /etc/grub2.cfg` and `grub2-mkconfig -o /etc/grub2-efi.cfg`.
-3. Do a `reboot`.
-
-## Firefox configuration
-Copy profile to `~/.var/app/org.mozilla.firefox/.mozilla/firefox`.  
-Do a manual install of https://github.com/rafaelmardojai/firefox-gnome-theme if profile import doesn't include it.
-
-## List of good Firefox extensions
-[µBlock Origin](https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/)  
-[Bitwarden](https://addons.mozilla.org/en-US/firefox/addon/bitwarden-password-manager/)  
-[I don't care about cookies](https://addons.mozilla.org/en-US/firefox/addon/i-dont-care-about-cookies/)  
-[Google Search Fixer](https://addons.mozilla.org/en-US/firefox/addon/google-search-fixer/) (for Android)
--- bin/install.sh
+++ bin/install.sh
@@ -18,23 +18,19 @@ gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'fi')]"
 # CONFIGURE PACKAGES #
 ######################
 
-# Install extra repositories
-sudo dnf -y install fedora-workstation-repositories
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y upgrade
+# Pacman init
+sudo pacman -Syu --noconfirm
 
-# Install additional Gnome packages with apt
-sudo dnf -y install \
+# Install additional Gnome packages with pacman
+sudo pacman -S \
+base-devel \
+code \
 htop \
-git \
 gcc \
 ufw \
 vim \
 unzip \
-unar \
+unrar \
 curl \
 wget \
 traceroute \
@@ -46,44 +42,37 @@ ncdu \
 mlocate \
 the_silver_searcher \
 moreutils \
-google-noto-emoji-fonts \
 papirus-icon-theme \
 neofetch \
 python3 \
-python3-pip \
-python-flake8 \
-fedmod \
-flatpak-module-tools \
-docker-ce \
-docker-ce-cli \
-containerd.io \
+python-pip \
+flake8 \
+docker \
 docker-compose \
+noto-fonts-emoji \
 gnome-tweak-tool \
+gnome-browser-connector \
 gnome-sound-recorder \
 pavucontrol \
-rhythmbox
+rhythmbox \
+inkscape \
+gimp \
+obs-studio \
+discord \
+telegram-desktop \
+steam
 
-# Install flatpak repos
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
+# Install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
-# Install GUI apps asx flatpaks
-flatpak install -y flathub org.gimp.GIMP
-flatpak install -y flathub org.inkscape.Inkscape
-flatpak install -y flathub org.telegram.desktop
-flatpak install -y flathub com.obsproject.Studio
-flatpak install -y flathub com.visualstudio.code
-flatpak install -y flathub com.getpostman.Postman
-flatpak install -y flathub com.valvesoftware.Steam
-flatpak install -y flathub com.discordapp.Discord
-flatpak install -y flathub com.spotify.Client
-flatpak install -y flathub com.google.Chrome
+# Install google chrome
+yay -S google-chrome
 
 # Remove unnecessary preinstalled things
-sudo dnf -y remove \
-firefox \
+sudo pacman -R --noconfirm \
 cheese \
-gedit \
 gnome-weather \
 gnome-clocks \
 gnome-contacts \
@@ -92,20 +81,16 @@ gnome-maps \
 gnome-characters \
 gnome-font-viewer \
 gnome-tour \
-gnome-photos \
-libreoffice-*
+gnome-photos
 
 # Install vscode extensions
 code --install-extension piousdeer.adwaita-theme
 code --install-extension ms-python.python
 code --install-extension bmewburn.vscode-intelephense-client
-code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.vscode-typescript-next
 code --install-extension ecmel.vscode-html-css
 code --install-extension esbenp.prettier-vscode
-code --install-extension github.copilot
 code --install-extension ms-azuretools.vscode-docker
-code --install-extension visualstudioexptteam.vscodeintellicode
 code --install-extension bradlc.vscode-tailwindcss
 code --install-extension PKief.material-icon-theme
 code --install-extension shardulm94.trailing-spaces
@@ -143,20 +128,15 @@ gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgr
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
 
-# Edit Gnome terminal profile
-profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
-profile=${profile:1:-1}
-gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" default-size-columns 125
-gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" default-size-rows 36
-gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" scrollbar-policy "never"
-gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" cursor-shape "underline"
-
 # Change dock favourites
-gsettings set org.gnome.shell favorite-apps "['com.google.Chrome.desktop', 'org.gnome.Terminal.desktop', 'com.visualstudio.code.desktop', 'org.gnome.Nautilus.desktop', 'org.telegram.desktop.desktop']"
+gsettings set org.gnome.shell favorite-apps "[]"
 
 # Reorder apps
 gsettings reset org.gnome.shell app-picker-layout
 gsettings set org.gnome.desktop.app-folders folder-children ['']
+
+# Show window buttons
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 
 
 ################################
@@ -168,8 +148,8 @@ gsettings set org.gnome.desktop.wm.keybindings close "['<Super><Shift>q']"
 
 # Configure custom hotkeys
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/','/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'gnome-terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command '/usr/bin/gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'gnome-console'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command '/usr/bin/kgx'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Super>Return'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name 'gnome-files'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command '/usr/bin/nautilus'
@@ -190,13 +170,7 @@ dconf write /org/gnome/desktop/wm/keybindings/switch-windows-backward "['<Shift>
 # CONFIGURE DIRECTORIES AND FILES #
 ###################################
 
-# Remove unused home directories
-rm -rf /home/sami/Templates
-rm -rf /home/sami/Public
-rm -rf /home/sami/.mozilla
-
 # Setup directories
-mkdir -p /home/sami/.var/app/com.visualstudio.code/config/Code/User/
 mkdir /home/sami/.ssh
 chown sami /home/sami/.ssh
 chmod 700 /home/sami/.ssh
@@ -205,7 +179,7 @@ chmod 700 /home/sami/.ssh
 ln -sf /home/sami/.files/rc/bashrc /home/sami/.bashrc
 ln -sf /home/sami/.files/rc/nanorc /home/sami/.nanorc
 ln -sf /home/sami/.files/rc/gitconfig /home/sami/.gitconfig
-ln -sf /home/sami/.files/rc/code /home/sami/.var/app/com.visualstudio.code/config/Code/User/settings.json
+ln -sf /home/sami/.files/rc/code /home/sami/.config/Code\ -\ OSS/User/settings.json
 
 # Link the bare minimum dotfiles for the root user
 sudo ln -sf /home/sami/.files/rc/bashrc /root/.bashrc
@@ -223,8 +197,7 @@ ln -sf /home/sami/.files/rc/hidden /home/sami/.hidden
 # Set Nautilus settings
 gsettings set org.gnome.nautilus.preferences default-folder-viewer "list-view"
 gsettings set org.gnome.desktop.privacy remember-recent-files false
-gsettings set org.gnome.nautilus.preferences sort-directories-first true
-gsettings set org.gnome.nautilus.list-view default-visible-columns ['name', 'detailed_type', 'size', 'date_modified']
+gsettings set org.gnome.nautilus.list-view default-visible-columns "['name', 'detailed_type', 'size', 'date_modified']"
 
 # Disable lock screen notifications that turn on the screen
 gsettings set org.gnome.desktop.notifications show-in-lock-screen false
@@ -239,19 +212,16 @@ gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
 # Set timezone
 sudo timedatectl set-timezone Europe/Helsinki
 
-# Add user to docker group
-sudo groupadd docker
+# Config docker service
 sudo gpasswd -a sami docker
 sudo usermod -aG docker sami
 sudo systemctl restart docker
+sudo systemctl enable docker
 
 
 ###############
 # FINAL STEPS #
 ###############
-
-# Clear unnecessary dependencies
-sudo dnf -y autoremove
 
 clear
 echo ""
