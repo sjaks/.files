a49778b9eee293e9839d34f9954f3564c8f0bae0
Sami Jakonen <sami.jakonen@iki.fi>
2024-10-09 21:43:33 +0300

    Create a custom ZSH theme

--- bin/links.sh
+++ bin/links.sh
@@ -1,3 +1,4 @@
 ln -sf /Users/samijakonen/.files/rc/zshrc /Users/samijakonen/.zshrc
 ln -sf /Users/samijakonen/.files/rc/code /Users/samijakonen/Library/Application\ Support/Code/User/settings.json
 ln -sf /Users/samijakonen/.files/rc/gitconfig /Users/samijakonen/.gitconfig
+ln -sf /Users/samijakonen/.files/rc/shell /Users/samijakonen/.oh-my-zsh/themes/custom.zsh-theme
